94d2ce32fa95b4fbf392d118b41299fa08bcd913
RowanFayez <fayezrowan5@gmail.com>
2026-05-18 20:49:21 +0200

    new base url deploment (temp)

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -3,11 +3,11 @@ class ApiConstants {
 
   /// Backend base URL (Railway).
   static const String baseUrl =
-      'https://routing-api-production-54ce.up.railway.app';
+      'https://alexandria-multimodal-routing-engine-production.up.railway.app';
 
   /// DB-tools base URL (Railway).
   static const String dbToolsBaseUrl =
-      'https://db-tools-production-6513.up.railway.app';
+      'https://dbtools-production.up.railway.app';
 
   static const String routesEndpoint = '/api/v1/journeys';
 
