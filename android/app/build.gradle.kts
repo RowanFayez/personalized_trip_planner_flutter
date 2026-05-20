715387eeec99dff8c327fd57be2ce21687f7dec8
RowanFayez <fayezrowan5@gmail.com>
2026-05-20 19:12:11 +0200

    fix: update package namespace and application to fix google cloud sha1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.nextstation"
+    namespace = "com.nextstation.app"
     compileSdk = flutter.compileSdkVersion
     // Keep in sync with plugins requiring newer NDK.
     ndkVersion = "27.0.12077973"
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.nextstation"
+        applicationId = "com.nextstation.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/nextstation/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/nextstation/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.nextstation
+package com.nextstation.app
 
 import io.flutter.embedding.android.FlutterActivity
 
